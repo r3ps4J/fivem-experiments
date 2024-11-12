ff11749ff5dc569d5237e202ebbeafc6af1eb324
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-11-12 17:45:48 +0100

    Only pack lua files and dist folder if they exist

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -3,7 +3,18 @@
 cd "resources/$1" || exit
 mkdir -p "./temp/$1"
 cp ./{fxmanifest.lua,README.md} "./temp/$1"
-cp ./*.lua "./temp/$1" || :
-cp -r ./dist "./temp/$1" || :
+
+if compgen -G ./*.lua; then
+    cp ./*.lua "./temp/$1"
+else
+    echo "No lua files found to pack"
+fi
+
+if [ -d "./dist" ]; then
+    cp -r ./dist "./temp/$1"
+else
+    echo "No dist folder found to pack"
+fi
+
 cd ./temp && zip -r "../$1.zip" "./$1"
 cd .. && rm -rf ./temp
